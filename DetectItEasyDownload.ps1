f88ef3b4e7c4270981bbce322a55294d205748b5
REIDEN <75898806+NOTREIDEN@users.noreply.github.com>
2022-11-21 16:31:01 -0300

    Add files via upload

---
+++ DetectItEasyDownload.ps1
@@ -0,0 +1,7 @@
+$URL="https://cdn.discordapp.com/attachments/999086212490985523/1026074241134755981/die_win64_portable_3.06.zip"
+$Path="./"
+Start-BitsTransfer -Source $URL -Destination $Path
+
+$URL="https://cdn.discordapp.com/attachments/999086212490985523/1026074396911210557/die_win32_portable_3.06.zip"
+$Path="./"
+Start-BitsTransfer -Source $URL -Destination $Path
---
+++ REIDENForensics.ps1
@@ -0,0 +1,64 @@
+$URL="https://cdn.discordapp.com/attachments/935642187142660177/935643654733848586/DRE-Files.exe"
+$Path="./"
+Start-BitsTransfer -Source $URL -Destination $Path
+
+$URL="https://cdn.discordapp.com/attachments/968614469410365461/968638851365093407/MFTExplorer.exe"
+$Path="./"
+Start-BitsTransfer -Source $URL -Destination $Path
+
+$URL="https://cdn.discordapp.com/attachments/968614469410365461/968638851604176927/TimelineExplorer.zip"
+$Path="./"
+Start-BitsTransfer -Source $URL -Destination $Path
+
+$URL="https://f001.backblazeb2.com/file/EricZimmermanTools/net6/ShellBagsExplorer.zip"
+$Path="./"
+Start-BitsTransfer -Source $URL -Destination $Path
+
+$URL="https://f001.backblazeb2.com/file/EricZimmermanTools/net6/SDBExplorer.zip"
+$Path="./"
+Start-BitsTransfer -Source $URL -Destination $Path
+
+$URL="https://f001.backblazeb2.com/file/EricZimmermanTools/net6/SrumECmd.zip"
+$Path="./"
+Start-BitsTransfer -Source $URL -Destination $Path
+
+$URL="https://f001.backblazeb2.com/file/EricZimmermanTools/net6/WxTCmd.zip"
+$Path="./"
+Start-BitsTransfer -Source $URL -Destination $Path
+
+$URL="https://f001.backblazeb2.com/file/EricZimmermanTools/AmcacheParser.zip"
+$Path="./"
+Start-BitsTransfer -Source $URL -Destination $Path
+
+$URL="https://f001.backblazeb2.com/file/EricZimmermanTools/AppCompatCacheParser.zip"
+$Path="./"
+Start-BitsTransfer -Source $URL -Destination $Path
+
+$URL="https://f001.backblazeb2.com/file/EricZimmermanTools/RegistryExplorer.zip"
+$Path="./"
+Start-BitsTransfer -Source $URL -Destination $Path
+
+$URL="https://download2275.mediafire.com/aq8jnit6huig/3p5my06frrscewt/%24I30.rar"
+$Path="./"
+Start-BitsTransfer -Source $URL -Destination $Path
+
+$URL="https://download2295.mediafire.com/8rdt8zqtt9dg/fk4xet720l502wt/Journal+Alternative.rar"
+$Path="./"
+Start-BitsTransfer -Source $URL -Destination $Path
+
+$URL="https://f001.backblazeb2.com/file/EricZimmermanTools/AmcacheParser.zip"
+$Path="./"
+Start-BitsTransfer -Source $URL -Destination $Path
+
+$URL="https://f001.backblazeb2.com/file/EricZimmermanTools/AmcacheParser.zip"
+$Path="./"
+Start-BitsTransfer -Source $URL -Destination $Path
+
+$URL="https://f001.backblazeb2.com/file/EricZimmermanTools/net6/MFTECmd.zip"
+$Path="./"
+Start-BitsTransfer -Source $URL -Destination $Path
+
+$URL="https://download1507.mediafire.com/hl9010jm0apg/xq20c2gxweppcin/REIDENForensics.rar"
+$Path="./"
+Start-BitsTransfer -Source $URL -Destination $Path
+
